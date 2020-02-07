308c2d3685b55e14b98ee98ea6c5a9b0dd85f12d
SkripMen <48603232+SkripMen@users.noreply.github.com>
2020-02-07 09:57:22 +0400

    Add files via upload

--- TL/index.php
+++ TL/index.php
@@ -3,42 +3,38 @@
 include ('connectBD.php');
 
 //Вывод формы для добавления книги
-echo <<<HTML
-<!DOCTYPE html>
-<html lang="en">
+echo '
 <head>
-    <meta charset="UTF-8">
     <title>Главная</title>
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
+<div class="section" data-vide-bg="video/ocean">
     <div class="container1">
-    <form method="POST" action="addpage.php">
+    <form action="addpage.php">
         <input name="name" placeholder="Название книги">
         <input name="price" placeholder="Цена">
         <input name="name_a" placeholder="Имя автора">
-
-        <button type="submit" class="but">Добавить книгу</button>
+        <button type="submit" data-tooltip="Добавление книги" class="but" id="pls"><i class="fa fa-plus-square-o" aria-hidden="true"></i></button>
     </form>
     <form action="delete_book.php">
     <button type="submit" data-tooltip="Удаление книги" class="but" id="del"><i class="fa fa-trash" aria-hidden="true"></i></button>
     </form>
-</div>
-</div>
-
-    <!-- Подключение шрифтов -->
-<script src="https://use.fontawesome.com/74811d09e3.js"></script>
-HTML;
+';
 
 $sql = $pdo->query('SELECT books.*, authors.* FROM books,authors,books_authors WHERE books_authors.id_books = books.id AND books_authors.id_author = authors.id');
 $sql = $sql->fetchAll();
-//var_dump($sql);
 foreach ($sql as $row){
     echo '<div class="book">'.$row['title'].' ';
     echo $row['price'].' ';
     echo $row['name'].'<br></div>';
 }
-
-//вызов функции вывода всех записей
-/*
-$mysql->getAllPost();*/
+echo '
+</div>
+</div>
+    <!-- Подключение шрифтов -->
+<script src="https://use.fontawesome.com/74811d09e3.js"></script>
+<script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="js/jquery.vide.min.js"></script>
+<div class="cop">Создано при поддержки АКВТ©</div>
+';
